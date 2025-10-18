correct for causal true

tests/test_ring_attention.py
@@ -83,31 +83,39 @@ class RingAttentionTest(unittest.TestCase):
         q_total = torch.load('q.pt')
         k_total = torch.load('k.pt')
         v_total = torch.load('v.pt')
+        
+        #batch_size, total_seq_len, num_heads, head_dim = q_total.shape
+        batch_size, num_heads, total_seq_len, head_dim = q_total.shape
+        seq_dim = 2
 
         rank = get_sequence_parallel_rank()
         q_total = q_total.to(torch.device(rank))
         k_total = k_total.to(torch.device(rank))
         v_total = v_total.to(torch.device(rank))
 
-        q_total_trans = q_total.transpose(1, 2)
-        k_total_trans = k_total.transpose(1, 2)
-        v_total_trans = v_total.transpose(1, 2)
+        q_total_trans = q_total.transpose(1, 2).contiguous()
+        k_total_trans = k_total.transpose(1, 2).contiguous()
+        v_total_trans = v_total.transpose(1, 2).contiguous()
 
-        batch_size, total_seq_len, num_heads, head_dim = q_total.shape
 
-        q = q_total.chunk(self.world_size, dim=1)[self.rank].contiguous()
-        k = k_total.chunk(self.world_size, dim=1)[self.rank].contiguous()
-        v = v_total.chunk(self.world_size, dim=1)[self.rank].contiguous()
+        q = q_total.chunk(self.world_size, dim=seq_dim)[self.rank].contiguous()
+        k = k_total.chunk(self.world_size, dim=seq_dim)[self.rank].contiguous()
+        v = v_total.chunk(self.world_size, dim=seq_dim)[self.rank].contiguous()
+        
+        q.trans = q.transpose(1, 2).contiguous()
+        k.trans = k.transpose(1, 2).contiguous()
+        v.trans = v.transpose(1, 2).contiguous()
 
-        causal = False
+
+        causal = True
         window_size = (-1, -1)
         softmax_scale=head_dim ** -0.5
 
         attn_output_local = xFuserLongContextAttention()(
             None,
-            q,
-            k,
-            v,
+            q.trans,
+            k.trans,
+            v.trans,
             dropout_p=0.0,
             causal=causal,
             softmax_scale=softmax_scale,
@@ -117,7 +125,7 @@ class RingAttentionTest(unittest.TestCase):
         attn_output = get_sp_group().all_gather(attn_output_local, dim=1)
 
         from flash_attn import flash_attn_func
-        sdpa_output = flash_attn_func(q_total, k_total, v_total,
+        sdpa_output = flash_attn_func(q_total_trans, k_total_trans, v_total_trans,
                                       causal=causal,
                                       dropout_p=0.0,
                                       softmax_scale=softmax_scale)